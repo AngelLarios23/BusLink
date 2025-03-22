e5dfef44b8fe8b32e1ca5bb4ceb07f37beab73bf
AngelLarios23 <angel306070@gmail.com>
2025-03-22 02:17:53 -0600

    BusLinkBeta1.5

--- frontend/App.js
+++ frontend/App.js
@@ -2,14 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { LinearGradient } from 'expo-linear-gradient';
-
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Login from './screens/Login';
 import Home from './screens/Home';
 import Registro from './screens/Registro';
 import Payment from './screens/Payment';
-import Diseno from './screens/diseno';
 
 export default function App() {
 
@@ -36,7 +34,6 @@ function MyStack() {
         headerStyle: {backgroundColor: "#525FE1"},
       }} />
       <Stack.Screen name='Payment' component={Payment}></Stack.Screen>
-      <Stack.Screen name='Diseno' component={Diseno}></Stack.Screen>
     </Stack.Navigator>
   );
 }
